3e349e2c7577050c5a24ddd022ababf509f81c94
PotatoBone <amdavid@mail.csuchico.edu>
2023-09-13 11:03:36 -0700

    Add test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x = 0;
+	int beta = 002;
+	cout << beta << endl;
+	int *ptr = &x;
+	cout << *ptr << endl;
+	x++;
+	cout << x << endl;
+	cout << *ptr << endl;
+	return 0;
+}
